281545f9f73e97e399f9e7ef9093f66928851ff3
xiangyu <xiangyuzhang365@gmail.com>
2016-05-11 12:13:48 -0400

    获取资源完成，生成html测试页面成功，服务器连接正常

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-// app是项目的main，它的责任是
+// app是项目的main
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var config = require('./config');
+// *部署资源
+// * 1 连接数据库
+mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+// * 2 获取资源
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+// 设置RESTful API
 // 一，生成页面，get
 app.get('/',function(req, res){
 	// 在响应函数里面发送html资源
+	res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 // 二，响应生成短链接的请求，post
 app.post('/api/shorten', function(req, res){
@@ -18,4 +41,12 @@ app.get('/:short_url', function(req,res){
 	// 1，查询数据库是否存在这样的short_url
 	// 2，解码短URL
 	// 3，重定向
+});
+
+
+var server = app.listen(3000, function(){
+	console.log('Thank you for using URL Shortener');
+	console.log('This application is used for Liaoyuan Test Only');
+	console.log('Author: Xiangyu Zhang');
+  	console.log('Server listening on port 3000');
 });
