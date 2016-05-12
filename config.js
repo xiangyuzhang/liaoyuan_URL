103111645fac1343839a8b9f17172493c275f04f
xiangyu <xiangyuzhang365@gmail.com>
2016-05-12 02:30:38 -0400

    publich

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'http://localhost/';
 config.liaoyuan = 'https://liaoyuan.io';
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
