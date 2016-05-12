0d7057b496331a7a02f07050ec99a5d9ed093772
xiangyu <xiangyuzhang365@gmail.com>
2016-05-12 15:29:22 -0400

    完成

--- README.mk
+++ README.mk
@@ -1,52 +1,18 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<title>用户手册</title>
-<style>
-rect {
-transition: .6s fill;
-fill: #D3D3D3;
-opacity: 0;
-}
-rect:hover {
-    fill: #D3D3D3;
-    opacity: 0.2;
-}
+部署方法
 
-.outline {
-    clear: both;
-}
+默认的环境是已经安装 NPM, NodeJS (或者 Node,视平台而定),还有 MongoDB。
 
-.svg-container {
-    width: 100%;
-    max-width: 2528px;
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-}
+1. 拷贝仓库 https://github.com/xiangyuzhang/liaoyuan_URL.git 
+2. 安装依赖 
+	$ npm install express –save 
+	$ npm install body-parser --save 
+	$ npm install mongoose –save 
+3. 打开并创建数据库 在终端输入 
+	$ mongo $ use url_shortener (在程序里,我默认数据库是 url_shortener, 可以 在配置文件 config.js 里面修改,但不建议)
+	$ db.counters.insert({_id:’url_count’, seq:1})(这一步是为了创建 一个 counter,我尝试过在后端自动创建,但是似乎不行。在数据库里面 直接创建可以解决这个问题)
+4. 根据需求更改配置文件 config.js 
+5. 运行: 
+	$ node(或者 nodejs) app.js
 
-.svg-content {
-    width: 100%;
-}
-
-.container {
-    width: 100%;
-}
-</style>
-
-</head>
-
-<body>
-
-<div class="container"><div class="spacer">&nbsp;</div><div class="svg-container"><svg version="1.1"  preserveAspectRatio="xMinYMin meet" class="svg-content" viewBox="0 0 2528 764" xmlns="//www.w3.org/2000/svg" xmlns:xlink="//www.w3.org/1999/xlink">
-<image width="2528" height="764" xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C.png"></image>
-<a xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/E5E7DEA0-47BF-432F-A5AA-6E968ACD9642/image.png" xlink:title="image.png"><rect x="30" y="470" width="424" height="158"/></a>
-<a xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/BE59F94E-C162-4D85-B1E5-435CE5A07757/image.png" xlink:title="image.png"><rect x="336" y="660" width="124" height="74"/></a>
-</svg>
-</div>
-
-<br/>
-<a href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-outline.html"><img src="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/outline-link.png" alt="Outline View" title="Outline View" /><a>
-
-</body>
-</html>
+实例：
+	http://broccoli.ecs.umass.edu:3000
