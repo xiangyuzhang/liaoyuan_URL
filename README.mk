6db5a7a4056f830b9dec0eed14ab56e72189322d
xiangyu <xiangyuzhang365@gmail.com>
2016-05-12 13:57:37 -0400

    加入了README

---
+++ README.mk
@@ -0,0 +1,52 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>用户手册</title>
+<style>
+rect {
+transition: .6s fill;
+fill: #D3D3D3;
+opacity: 0;
+}
+rect:hover {
+    fill: #D3D3D3;
+    opacity: 0.2;
+}
+
+.outline {
+    clear: both;
+}
+
+.svg-container {
+    width: 100%;
+    max-width: 2528px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+
+.svg-content {
+    width: 100%;
+}
+
+.container {
+    width: 100%;
+}
+</style>
+
+</head>
+
+<body>
+
+<div class="container"><div class="spacer">&nbsp;</div><div class="svg-container"><svg version="1.1"  preserveAspectRatio="xMinYMin meet" class="svg-content" viewBox="0 0 2528 764" xmlns="//www.w3.org/2000/svg" xmlns:xlink="//www.w3.org/1999/xlink">
+<image width="2528" height="764" xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C.png"></image>
+<a xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/E5E7DEA0-47BF-432F-A5AA-6E968ACD9642/image.png" xlink:title="image.png"><rect x="30" y="470" width="424" height="158"/></a>
+<a xlink:href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/BE59F94E-C162-4D85-B1E5-435CE5A07757/image.png" xlink:title="image.png"><rect x="336" y="660" width="124" height="74"/></a>
+</svg>
+</div>
+
+<br/>
+<a href="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-outline.html"><img src="%E7%94%A8%E6%88%B7%E6%89%8B%E5%86%8C-assets/outline-link.png" alt="Outline View" title="Outline View" /><a>
+
+</body>
+</html>
